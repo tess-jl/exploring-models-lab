347e3678b6e84caa2f2c4877c3779773a483ffb8
Tess Lameyer <tjlameyer@gmail.com>
2019-12-06 14:25:15 -0800

    can connec to my server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('./lib/utils/connect.js')();
+require('dotenv').config();
+
 const app = require('./lib/app');
 
 app.listen('7890', () => {
-  console.log('started!');
+  console.log('started!!!!!!!!!!!!!!!');
 });
