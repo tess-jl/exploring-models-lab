7dd6fb479fba8ab7222acb581976206dd769b3d5
Tess Lameyer <tjlameyer@gmail.com>
2019-12-06 15:51:57 -0800

    test for post route is passing!!!!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
+require('../lib/utils/connect.js')();
 
 describe('application routes', () => {
   // it('has a post route that posts things', () => {
@@ -12,17 +13,23 @@ describe('application routes', () => {
 
   it('has a /postRoute post route', () => {
     return request(app)
-      .post('/hello')
+      .post('/postRoute')
       .send({
-        name: 'spot',
-        age: 5,
-        weight: '20 lbs'
+        name: 'maple',
+        leaves: 300, 
+        roots: 300, 
+        branches: 3, 
+        trunk: 'not chopped down'
       })
       .then(res => {
         expect(res.body).toEqual({
-          name: 'spot',
-          age: 5,
-          weight: '20 lbs'
+          '__v': 0,
+          '_id': expect.any(String),
+          name: 'maple',
+          leaves: 300, 
+          roots: 300, 
+          branches: 3, 
+          trunk: 'not chopped down'
         });
       });
   });
