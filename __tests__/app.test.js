30d79f891dd3967e7eea1e39199333a526ff368f
Tess Lameyer <tjlameyer@gmail.com>
2019-12-06 15:14:35 -0800

    wrote my first post route to post a new Tree

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,29 +1,29 @@
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-// describe('application routes', () => {
-//   it('has a home route that says hello everyone', () => {
-//     return request(app)
-//       .get('/')
-//       .then(res => {
-//         expect(res.body).toEqual({ text: 'hello' });
-//       });
-//   });
+describe('application routes', () => {
+  // it('has a post route that posts things', () => {
+  //   return request(app)
+  //     .get('/')
+  //     .then(res => {
+  //       expect(res.body).toEqual({ text: 'hello' });
+  //     });
+  // });
 
-//   it('has a /hello post route', () => {
-//     return request(app)
-//       .post('/hello')
-//       .send({
-//         name: 'spot',
-//         age: 5,
-//         weight: '20 lbs'
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           name: 'spot',
-//           age: 5,
-//           weight: '20 lbs'
-//         });
-//       });
-//   });
-// });
+  it('has a /postRoute post route', () => {
+    return request(app)
+      .post('/hello')
+      .send({
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'spot',
+          age: 5,
+          weight: '20 lbs'
+        });
+      });
+  });
+});
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('./lib/utils/connect.js')();
 require('dotenv').config();
 
+//our server makes queries to db, mongo db sends data back 
+
 const app = require('./lib/app');
 
 app.listen('7890', () => {
