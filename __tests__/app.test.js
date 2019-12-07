586fe894c126068aaa294b28aee895228c6e0504
Tess Lameyer <tjlameyer@gmail.com>
2019-12-06 17:06:59 -0800

    tests are passing all using the same id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,8 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 require('../lib/utils/connect.js')();
 
+let _id; 
+
 describe('application routes', () => {
-  it('has a /postRoute post route', () => {
+  it('has a /postRoute post route that creates an object', () => {
     return request(app)
       .post('/postRoute')
       .send({
@@ -14,6 +16,8 @@ describe('application routes', () => {
         trunk: 'not chopped down'
       })
       .then(res => {
+        _id = res.body._id;
+        console.log(_id, 'this is the _iddddd');
         expect(res.body).toEqual({
           '__v': 0,
           '_id': expect.any(String),
@@ -29,10 +33,11 @@ describe('application routes', () => {
   it('has a get route that gets things', () => {
     return request(app)
     //copy pasted the id from the compass monogo db interface
-      .get('/5deae0345b12221e7e85e1a9')
+      .get(`/${_id}`)
       .then(res => {
-        expect(res.body).toEqual({  '__v': 0,
-          '_id': '5deae0345b12221e7e85e1a9',
+        expect(res.body).toEqual({  
+          '__v': 0,
+          '_id': `${_id}`,
           'branches': 3,
           'leaves': 300,
           'name': 'maple',
@@ -43,13 +48,14 @@ describe('application routes', () => {
 
   it('has an update route that updates things', () => {
     return request(app)
-      .get('/5deae0345b12221e7e85e1a9')
+      .get(`/${_id}`)
       .send({
         name: 'elm'
       })
       .then(res => {
-        expect(res.body).toEqual({  '__v': 0,
-          '_id': '5deae0345b12221e7e85e1a9',
+        expect(res.body).toEqual({  
+          '__v': 0,
+          '_id': `${_id}`,
           'branches': 3,
           'leaves': 300,
           'name': 'maple',
@@ -57,4 +63,22 @@ describe('application routes', () => {
           'trunk': 'not chopped down', });
       });
   });
+
+  // it('has a delete route that deletes things', () => {
+  //   return request(app)
+  //     .get(`/${_id}`)
+  //     .send({
+  //       name: 'elm'
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({  
+  //         '__v': 0,
+  //         '_id': `${_id}`,
+  //         'branches': 3,
+  //         'leaves': 300,
+  //         'name': 'maple',
+  //         'roots': 300,
+  //         'trunk': 'not chopped down', });
+  //     });
+  // });
 });
